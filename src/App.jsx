8565f6081343cbe53eca9963df44843f37658e9b
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-19 14:23:11 +0200

    imported apiFacade.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import OpenSearch from "./components/OpenSearch.jsx";
 
 function App() {
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <>
       <h1>Hello World</h1>
+      <OpenSearch />
     </>
   );
 }
---
+++ src/components/OpenSearch.jsx
@@ -0,0 +1,23 @@
+
+export default function OpenSearch() {
+  return (
+    <>
+      <h1>Open Search</h1>
+    </>
+  );
+}
+
+
+
+
+
+// async function fetchOpenSearch(text) {
+//   const response = await fetch(
+//     "https://tripapi.cphbusinessapps.dk/api/trips/" + id,
+//     api.makeOptions("GET", true)
+//   );
+//   const tripDetails = await response.json();
+//   console.log(tripDetails);
+
+//   return tripDetails;
+// }
