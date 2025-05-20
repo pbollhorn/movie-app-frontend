72e3099b14e3a515fb2e857fc81daee86e26b5f0
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-20 12:05:32 +0200

    progression in MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,22 @@
+import styles from "./MovieCard.module.css";
+
 export default function MovieCard({ movieData }) {
   return (
-    <div>
-      <h1>{movieData.title}</h1>
+    <div className={styles.movieCard}>
       <img
         src={"https://image.tmdb.org/t/p/w500/" + movieData.posterPath}
       ></img>
+      <div>
+        <h1>{movieData.title}</h1>
+        <h1>{movieData.originalTitle}</h1>
+        <h1>
+          {"" +
+            movieData.releaseDate[0] +
+            " Rating: " +
+            movieData.rating +
+            "/10"}
+        </h1>
+      </div>
     </div>
   );
 }
