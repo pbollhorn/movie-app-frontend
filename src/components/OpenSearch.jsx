c7896ed222457186538da4c37e0f8ee547a5432c
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-20 10:48:09 +0200

    progression in search

--- src/components/OpenSearch.jsx
+++ src/components/OpenSearch.jsx
@@ -1,25 +1,39 @@
+import { useRef } from "react";
 import api from "../apiFacade.js";
 
 export default function OpenSearch() {
+  const searchTextRef = useRef(null);
+
   // console.log(
   //   api.fetchData("movies/search-open?text=olsen", api.makeOptions("GET", true))
   // );
 
-  fetchOpenSearch("hello");
+  async function handleOpenSearchSubmit(event) {
+    event.preventDefault();
+    console.log(searchTextRef.current.value);
+    const text = searchTextRef.current.value;
+
+    const data = await fetchOpenSearch(text);
+
+    console.log(data);
+  }
 
   return (
     <>
       <h1>Open Search</h1>
+      <form onSubmit={handleOpenSearchSubmit}>
+        <label>Search in title or original title: </label>
+        <input type="text" ref={searchTextRef} />
+        <button type="submit">Search</button>
+      </form>
     </>
   );
 }
 
 async function fetchOpenSearch(text) {
   const response = await fetch(
-    "https://movie.jcoder.dk/api/movies/search-open?text=olsen"
+    "https://movie.jcoder.dk/api/movies/search-open?text=" + text
   );
   const data = await response.json();
-  console.log(data);
-
   return data;
 }
