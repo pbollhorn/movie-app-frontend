c7e945f6366b5360835eeff8105c2ec84b3b8ea7
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-20 14:33:07 +0200

    Opinions.jsx works now

--- src/components/Opinions.jsx
+++ src/components/Opinions.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import MovieCardList from "./MovieCardList.jsx";
+import api from "../apiFacade.js";
+
 export default function Opinions() {
+  const [list, setList] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    api.fetchData("movies", api.makeOptions("GET", true)).then((data) => {
+      console.log(data);
+      setList(data);
+    });
+  }, []); // Runs on mount
+
   return (
     <>
       <h1>My Opinions</h1>
+      <MovieCardList list={list} />
     </>
   );
 }
