b6d86adf04aaeb6dadf8a97c7569c551edaa0889
Peter Bollhorn <pbollhorn@gmail.com>
2025-05-21 16:58:58 +0200

    implemented light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
+  
+  // useEffect for setting dark/light mode according to browser settings
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (mediaQuery.matches) {
+      document.body.className = "dark";
+    } else {
+      document.body.className = "light";
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
