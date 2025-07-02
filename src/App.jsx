10e1dbc913e2a7e80e5feef900b4df323735781f
Peter Bollhorn <pbollhorn@gmail.com>
2025-07-02 08:35:54 +0200

    progression

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ export default function App() {
 
   const [activeMovieId, setActiveMovieId] = useState(null);
 
+  // Mobile view
   if (windowWidth < 768) {
     return (
       <div>
@@ -39,6 +40,9 @@ export default function App() {
     );
   }
 
+  // Tablet view
+
+  // Desktop view
   return (
     <div className={styles.desktopView}>
       <Sidebar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
