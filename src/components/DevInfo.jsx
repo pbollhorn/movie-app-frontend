a3b262f1715ee3726b10be2cabb7413581b674d9
Peter Bollhorn <pbollhorn@gmail.com>
2025-08-08 14:26:18 +0200

    improved DevInfo.jsx

--- src/components/DevInfo.jsx
+++ src/components/DevInfo.jsx
@@ -1,19 +1,30 @@
 export default function DevInfo() {
+  const remInPx = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+  );
+
   return (
     <div>
-      {"screen.width: " + screen.width}
+      <h1>Dev Info</h1>
+      <h2>Display</h2>
+      (Remember to reload after resizing)
       <br />
-      {"screen.height: " + screen.height}
+      <br />
+      {"screen.width: " + screen.width + " px"}
+      <br />
+      {"screen.height: " + screen.height + " px"}
       <br />
       {"window.devicePixelRatio: " + window.devicePixelRatio}
       <br />
-      {"window.innerWidth: " + window.innerWidth}
+      {"window.innerWidth: " + window.innerWidth + " px"}
       <br />
-      {"window.outerWidth: " + window.outerWidth}
+      {"window.outerWidth: " + window.outerWidth + " px"}
       <br />
-      {"window.innerHeight: " + window.innerHeight}
+      {"window.innerHeight: " + window.innerHeight + " px"}
       <br />
-      {"window.outerHeight: " + window.outerHeight}
+      {"window.outerHeight: " + window.outerHeight + " px"}
+      <br />
+      {"1 rem: " + remInPx + " px"}
       <br />
     </div>
   );
