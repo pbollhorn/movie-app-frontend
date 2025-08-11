fdb42c258d7c64d2a122f6df30fba1181fc0603a
Peter Bollhorn <pbollhorn@gmail.com>
2025-08-11 21:57:25 +0200

    search button now uses svg

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -23,8 +23,11 @@ export default function Search() {
       <h1>Search Movies</h1>
       <form onSubmit={handleSearchSubmit}>
         <input type="text" ref={searchTitleRef} placeholder="Search by title" />
-        <button type="submit">Search</button>
+        {/* <button type="submit">Search</button> */}
         {/* <button type="submit">🔍</button> */}
+        <button type="submit">
+          <img src={BurgerButton} style={{ height: "1rem" }}/>
+        </button>
       </form>
       <MovieCardList list={list} setActiveMovieId={setActiveMovieId} />
     </>
