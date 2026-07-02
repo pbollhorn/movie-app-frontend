c9b168b0ebabf1351b5c65288b3f347ff3c8b0fb
Peter Bollhorn <pbollhorn@gmail.com>
2026-07-02 15:42:16 +0200

    Top100.jsx now fetches genreList instead of having it hardcoded

--- src/components/pages/Top100.jsx
+++ src/components/pages/Top100.jsx
@@ -4,11 +4,20 @@ import api from "../../apiFacade.js";
 import MovieList from "../MovieList.jsx";
 
 export default function Top100() {
+  const [genreList, setGenreList] = useState(null);
   const [list, setList] = useState([]);
   const [params, setParams] = useSearchParams();
   const genreIdParam = params.get("genreId") || "";
 
   const { setActiveMovieId } = useOutletContext();
+
+  // Fetch genreList on mount
+  useEffect(() => {
+    api.fetchData("genres", api.makeOptions("GET", true)).then((data) => {
+      console.log(data);
+      setGenreList(data);
+    });
+  }, []);
 
   // Fetch data automatically whenever the URL query parameter "genreId" changes
   useEffect(() => {
@@ -31,31 +40,25 @@ export default function Top100() {
     }
   }
 
+  if (!genreList) {
+    return (
+      <>
+        <h1>Top 100 Movies</h1>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Top 100 Movies</h1>
 
       <select value={genreIdParam} onChange={handleGenreSelect}>
         <option value="">All Genres</option>
-        <option value="28">Action</option>
-        <option value="12">Adventure</option>
-        <option value="16">Animation</option>
-        <option value="35">Comedy</option>
-        <option value="80">Crime</option>
-        <option value="99">Documentary</option>
-        <option value="18">Drama</option>
-        <option value="10751">Family</option>
-        <option value="14">Fantasy</option>
-        <option value="36">History</option>
-        <option value="27">Horror</option>
-        <option value="10402">Music</option>
-        <option value="9648">Mystery</option>
-        <option value="10749">Romance</option>
-        <option value="878">Sci-Fi</option>
-        <option value="53">Thriller</option>
-        <option value="10770">TV Movie</option>
-        <option value="10752">War</option>
-        <option value="37">Western</option>
+        {genreList.map((g) => (
+          <option key={g.id} value={g.id}>
+            {g.name}
+          </option>
+        ))}
       </select>
 
       <MovieList
